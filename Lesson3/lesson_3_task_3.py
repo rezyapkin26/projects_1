a899ec7bb0164d5c120e34b2514679338914654b
rezyapkin26 <rezyapkin.petr1211@yandex.ru>
2024-12-03 15:30:38 +0300

    Исправление

--- Lesson3/lesson_3_task_3.py
+++ Lesson3/lesson_3_task_3.py
@@ -1,5 +1,5 @@
 from Address import Address
-from Lesson3.Mailing import Mailing
+from Mailing import Mailing
 to_address = Address("1231312", "Видное", 'Крымская', '5','8')
 from_address = Address('231231412', 'Ковылкино', 'Толстого', '24', '1')
 mailing = Mailing( to_address, from_address, 400, 'trask987654')
