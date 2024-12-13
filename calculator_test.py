222148ec51635b7d45a7af5cdddc72232861b407
rezyapkin26 <rezyapkin.petr1211@yandex.ru>
2024-12-13 14:09:17 +0300

    test

--- calculator_test.py
+++ calculator_test.py
@@ -1,40 +1,95 @@
+import pytest
 from calculator import Calculator
 calculator = Calculator()
-# + +
-# - - 
-# - +
-# , , 
-# n + 0
-print('start')
-res = calculator.sum(4,5)
-assert res == 9
 
-res = calculator.sum(-6, -10)
-assert res == -16
+@pytest.mark.positive_test
+def test_sum_positive_nums():
+    calculator = Calculator()
+    res = calculator.sum(4,5)
+    assert res == 9
 
-res = calculator.sum(-6, 6)
-assert res == 0
+def test_sum_negative_nums():
+    calculator = Calculator()
+    res = calculator.sum(-6,-10)
+    assert res == -16
 
-res = calculator.sum(5.6, 4.3)
-res = round(res,1)
-assert res == 9.9
+def test_sum_positive_and_negative_nums():
+    calculator = Calculator()
+    res = calculator.sum(-6,6)
+    assert res == 0
 
-res = calculator.sum(10, 0)
-assert res == 10
+def test_sum_float_nums():
+    calculator = Calculator()
+    res = calculator.sum(5.6, 4.3)
+    res = round(res, 1)
+    assert res == 9.9
 
-res = calculator.div(10, 2)
-assert res == 5
+def test_sum_zero_nums():
+    calculator = Calculator()
+    res = calculator.sum(10, 0)
+    assert res == 10
 
-numbers = []
-res = calculator.avg(numbers)
-assert res == 0
+def test_div_positive():
+    calculator = Calculator()
+    res = calculator.div(10,2)
+    assert res == 5
 
-numbers = [1,2,3,4,5,6,7,8,9,5]
-res = calculator.avg(numbers)
-assert res == 5
-print('finish')
+def test_div_by_zero():
+    calculator = Calculator()
+    with pytest.raises(ArithmeticError):
+       calculator.div(10,0)
+
+def test_avg_emty_list():
+    calculator = Calculator()
+    numbers = []
+    res = calculator.avg(numbers)
+    assert res == 0
+
+def test_avg_positive():
+    calculator = Calculator()
+    numbers = [1,2,3,4,5,6,7,8,9,5]
+    res = calculator.avg(numbers)
+    assert res == 5
 
 
-res = calculator.div(10, 0)
-assert res == None
-print('finish')
+
+
+
+# # + +
+# # - - 
+# # - +
+# # , , 
+# # n + 0
+# print('start')
+# res = calculator.sum(4,5)
+# assert res == 9
+
+# res = calculator.sum(-6, -10)
+# assert res == -16
+
+# res = calculator.sum(-6, 6)
+# assert res == 0
+
+# res = calculator.sum(5.6, 4.3)
+# res = round(res,1)
+# assert res == 9.9
+
+# res = calculator.sum(10, 0)
+# assert res == 10
+
+# res = calculator.div(10, 2)
+# assert res == 5
+
+# numbers = []
+# res = calculator.avg(numbers)
+# assert res == 0
+
+# numbers = [1,2,3,4,5,6,7,8,9,5]
+# res = calculator.avg(numbers)
+# assert res == 5
+# print('finish')
+
+
+# res = calculator.div(10, 0)
+# assert res == None
+# print('finish')
